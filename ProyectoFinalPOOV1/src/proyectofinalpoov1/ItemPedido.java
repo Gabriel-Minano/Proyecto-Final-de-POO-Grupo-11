9a552f7f843bee3d7d7e1f07b31979abd1d99558
Gabriel Miñano <gabriel.minano.calderon@gmail.com>
2025-04-21 14:07:17 -0500

    versión 1.3c se eliminaron metodos sobrantes en ItemPedido

--- ProyectoFinalPOOV1/src/proyectofinalpoov1/ItemPedido.java
+++ ProyectoFinalPOOV1/src/proyectofinalpoov1/ItemPedido.java
@@ -20,17 +20,8 @@ public class ItemPedido {
         return producto;
     }
 
-    public void setProducto(Producto producto) {
-        this.producto = producto;
-    }
-
     public int getCantidad() {
         return cantidad;
     }
-
-    public void setCantidad(int cantidad) {
-        this.cantidad = cantidad;
-    }
-
 
 }
